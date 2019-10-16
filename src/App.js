01b55e663225984a6e590fc51fddde9d364d10ee
Przemyslaw Pokorski <pokora222@gmail.com>
2019-10-16 12:23:55 +0100

    Added stubAPI

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Header from "./components/header/";
+import ContactList from "./components/contactList/";
+import FilterControls from "./components/filterControls/";
+import request from "superagent";
+import api from "./dataStore/stubAPI"; // NEW
 
 class App extends Component {
+  componentDidMount() {
+    request.get("https://randomuser.me/api/?results=50").end((error, res) => {
+      if (res) {
+        let { results: contacts } = JSON.parse(res.text);
+        api.initialize(contacts);
+        this.setState({});
+      } else {
+        console.log(error);
+      }
+    });
+  }
   render() {
+    let contacts = api.getAll();
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <div className="jumbotron">
+          <Header noContacts={contacts.length} />
+          <FilterControls />
+          <ContactList contacts={contacts} />
+        </div>
     );
   }
 }
